c29fd8a733d6c827ae6729814534372d5faa14b0
kai <gmdezreal@gmail.com>
2022-02-28 11:18:28 -0600

    print version when --version flag is present

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::env;
+use std::process;
+
+const VERSION: &str = "0.1.0";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
+    
+    print_ver_if_necessary(&args);
+    
     let repeat_this = string_to_repeat(&args);
     loop {
         println!("{repeat_this}");
+    }
+}
+
+fn print_ver_if_necessary(args: &Vec<String>) {
+    if args.contains(&String::from("--version")) {
+        println!("{VERSION}");
+        process::exit(0);
     }
 }
 
