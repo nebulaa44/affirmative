bb22c1a920e2e9bbb2bbe62e0784a6f94fb4f8de
kai <gmdezreal@gmail.com>
2022-02-28 11:39:09 -0600

    refactor code

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,27 @@ use std::process;
 const VERSION: &str = "0.1.0";
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args_vec: Vec<String> = env::args().collect();
+
+    print_ver_if_necessary(&args_vec);
     
-    print_ver_if_necessary(&args);
-    
-    let repeat_this = string_to_repeat(&args);
+    let repeat_this = string_to_repeat(&args_vec);
     loop {
         println!("{repeat_this}");
     }
 }
 
-fn print_ver_if_necessary(args: &Vec<String>) {
-    if args.contains(&String::from("--version")) {
+fn print_ver_if_necessary(arg_vec: &Vec<String>) {
+
+    if arg_vec.contains(&String::from("--version")) {
         println!("{VERSION}");
         process::exit(0);
     }
 }
 
-fn string_to_repeat(args: &Vec<String>) -> String {
+fn string_to_repeat(arg_vec: &Vec<String>) -> String {
     // The first argument is always the path of the binary being run.
-    match args.get(1) {
+    match arg_vec.get(1) {
         None      => String::from("y"),
         Some(arg) => arg.to_owned()
     }
