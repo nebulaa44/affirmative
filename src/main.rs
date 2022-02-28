6dae3ee4954749df02b877437cbad4061b3d68dd
kai <gmdezreal@gmail.com>
2022-02-28 10:57:26 -0600

    repeat string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let repeat_this = string_to_repeat();
+    loop {
+        println!("{repeat_this}");
+    }
+}
+
+fn string_to_repeat() -> String {
+    let args: Vec<String> = env::args().collect();
+    
+    match args.get(1) {
+        None      => String::from("y"),
+        Some(arg) => arg.to_owned()
+    }
 }
