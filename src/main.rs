b8a4058724f2724c04ee993b51d6ed0861ff1b1a
kai <gmdezreal@gmail.com>
2022-02-28 11:10:56 -0600

    add a comment

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
 fn string_to_repeat() -> String {
     let args: Vec<String> = env::args().collect();
     
+    // The first argument is always the path of the binary being run.
     match args.get(1) {
         None      => String::from("y"),
         Some(arg) => arg.to_owned()
